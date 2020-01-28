a6da243dcb97604eafbee55025019ec15dd1788d
Brian Salemi <briandsalemi@gmail.com>
2020-01-28 14:53:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,6 +23,6 @@ end
 def add_supply_to_winter_holidays(holiday_supplies, supply)
   winter_holidays = holiday_supplies[:winter]
   winter_holidays.each do |holiday, supplies|
-    holiday << supply
+    holiday[:supplies] << supply
   end
 end
