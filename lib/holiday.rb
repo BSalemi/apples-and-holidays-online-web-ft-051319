4820031aa4850b6539178bc34db6aefbe453fbdd
Brian Salemi <briandsalemi@gmail.com>
2020-01-28 14:50:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,21 @@
 require 'pry'
 
+# holiday_supplies = {
+#   :winter => {
+#     :christmas => ["Lights", "Wreath"],
+#     :new_years => ["Party Hats"]
+#   },
+#   :summer => {
+#     :fourth_of_july => ["Fireworks", "BBQ"]
+#   },
+#   :fall => {
+#     :thanksgiving => ["Turkey"]
+#   },
+#   :spring => {
+#     :memorial_day => ["BBQ"]
+#   }
+# }
+
 def second_supply_for_fourth_of_july(hash)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
