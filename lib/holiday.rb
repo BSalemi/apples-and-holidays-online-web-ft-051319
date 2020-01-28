363767ec7d889a8e1d6d977a964b141cd0dfc919
Brian Salemi <briandsalemi@gmail.com>
2020-01-28 14:53:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,7 +21,7 @@ def second_supply_for_fourth_of_july(holiday_supplies)
 end
 
 def add_supply_to_winter_holidays(holiday_supplies, supply)
-  winter_holidays = holiday_supples[:winter]
+  winter_holidays = holiday_supplies[:winter]
   winter_holidays.each do |holiday, supplies|
     holiday << supply
   end
